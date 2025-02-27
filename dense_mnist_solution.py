661f1c472ec491f66b2f159208e663fccda2d05f
Amogh <axm0754@mavs.uta.edu>
2025-02-27 09:57:59 -0600

    Add function to create and train a neural network model in dense_mnist_solution.py

--- dense_mnist_solution.py
+++ dense_mnist_solution.py
@@ -27,3 +27,53 @@ def load_mnist() -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
     return training_inputs, training_labels, test_inputs, test_labels
 
 
+
+def create_and_train_model(training_inputs: np.ndarray, training_labels: np.ndarray, layers: int,
+                           units_per_layer: List[int], epochs: int, hidden_activations: List[str]) -> keras.models:
+    """
+    Creates and trains a neural network model.
+
+    Args:
+        training_inputs (np.ndarray): A 2D NumPy array where each row represents a training input vector.
+        training_labels (np.ndarray): A 2D NumPy array with a single column where
+            training_labels[i, 0] represents the class label for training_inputs[i].
+        layers (int): The total number of layers in the neural network.
+        units_per_layer (List[int]): A list specifying the number of units in each hidden layer.
+            The length of this list should be layers - 2.
+            For example, units_per_layer[0] corresponds to the number of units in the first hidden layer.
+        epochs (int): The number of training epochs.
+        hidden_activations (List[str]): A list of strings specifying the activation function for each hidden layer.
+            Possible values are 'tanh', 'sigmoid', and 'relu'.
+
+    Returns:
+        object: The trained model.
+    """
+
+    # Let Keras use its default method for initialization of all weights (i.e., your code should not address this issue at all).
+
+    input_shape = training_inputs[0].shape
+    number_of_classes = np.max([np.max(training_labels)]) + 1
+
+    # model = keras.Sequential(keras.Input(shape=input_shape), keras.layers.Dense(number_of_classes, activation='sigmoid'))
+
+    # model.add(keras.layers.Dense(units_per_layer[0], input_dim=training_inputs.shape[1], activation=hidden_activations[0]))
+
+    model_input = [keras.Input(shape=input_shape)]
+
+    for units, activation in zip(units_per_layer, hidden_activations):
+        model_input.append(keras.layers.Dense(units=units, activation=activation))
+
+    model_input.append(keras.layers.Dense(number_of_classes, activation='sigmoid'))
+
+    model = keras.Sequential(model_input)
+    #
+    # for units, activation in zip(units_per_layer, hidden_activations):
+    #     model.add(keras.layers.Dense(units=units, activation=activation))
+    #
+    # model.add(keras.layers.Dense(number_of_classes, activation='softmax'))
+    #
+    model.compile(optimizer='adam', loss=keras.losses.SparseCategoricalCrossentropy(), metrics=['accuracy'])
+
+    model.fit(training_inputs, training_labels, epochs=epochs)
+
+    return model
