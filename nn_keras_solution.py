cda1c0cd2eee5807d7e61a741c2bd6b480c66c3a
Amogh <axm0754@mavs.uta.edu>
2025-02-27 10:05:55 -0600

    Remove commented-out code and simplify model initialization in nn_keras_solution.py

--- nn_keras_solution.py
+++ nn_keras_solution.py
@@ -26,14 +26,8 @@ def create_and_train_model(training_inputs: np.ndarray, training_labels: np.ndar
         keras.models: The trained model.
     """
 
-    # Let Keras use its default method for initialization of all weights (i.e., your code should not address this issue at all).
-
     input_shape = training_inputs[0].shape
     number_of_classes = np.max([np.max(training_labels)]) + 1
-
-    # model = keras.Sequential(keras.Input(shape=input_shape), keras.layers.Dense(number_of_classes, activation='sigmoid'))
-
-    # model.add(keras.layers.Dense(units_per_layer[0], input_dim=training_inputs.shape[1], activation=hidden_activations[0]))
 
     model_input = [keras.Input(shape=input_shape)]
 
@@ -43,12 +37,7 @@ def create_and_train_model(training_inputs: np.ndarray, training_labels: np.ndar
     model_input.append(keras.layers.Dense(number_of_classes, activation='sigmoid'))
 
     model = keras.Sequential(model_input)
-    #
-    # for units, activation in zip(units_per_layer, hidden_activations):
-    #     model.add(keras.layers.Dense(units=units, activation=activation))
-    #
-    # model.add(keras.layers.Dense(number_of_classes, activation='softmax'))
-    #
+
     model.compile(optimizer='adam', loss=keras.losses.SparseCategoricalCrossentropy(), metrics=['accuracy'])
 
     model.fit(training_inputs, training_labels, epochs=epochs)
